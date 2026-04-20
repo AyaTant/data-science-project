2a2bf8c2ecf38cd117748ab8964d8cec45a4d8a2
Aya Tantaoui <ayatantaoui12@gmail.com>
2026-04-20 17:01:10 +0100

    Fix: Model trainer config error fixed

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -57,15 +57,16 @@ class ConfigurationManager:
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config=self.config.model_trainer
         params = self.params.ElasticNet
-        schema = self.schema.TARGET_COLUMN
+        schema = self.schema.TARGET_COLUMN.name
 
+        create_directories([config.root_dir])
         model_trainer_config = ModelTrainerConfig(
             root_dir = config.root_dir,
             train_data_path = config.train_data_path,
             test_data_path = config.test_data_path,
             model_name = config.model_name,
-            alpha = config.alpha,
-            l1_ratio = config.l1_ratio,
-            target_column = config.target_column
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema
         )
         return model_trainer_config
